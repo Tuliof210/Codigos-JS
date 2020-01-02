4e7599f70c42666731d8d490baeb334f595a4b93
Tulio Ferreira <tuliof210@gmail.com>
2020-01-02 16:25:56 -0300

    Nodemon

---
+++ NodeCodes/ModulosTerceiros.js
@@ -0,0 +1,6 @@
+const _ = require('lodash')
+
+setInterval(() => console.log(_.random(1, 1000)), 1000)
+
+//instalar via npm o lodash e o nodemon
+//nodemon serve para dar refresh no codigo em execução assim que houver uma mudança
